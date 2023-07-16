0542baf44ccce1d7fcadb9a30fa0b6a74defffc4
etienne <em85@pm.me>
2023-07-16 15:42:49 +0000

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-// import { RemoteGpio } from "./RemoteGpio";
-
 import React, { useState } from "react";
 import { EspDashboard } from "./EspDashboard";
 import "./index.css"
-// import { GpioStatesMonitor } from "./GpioStatesMonitor";
-// import { RovRemoteControl } from "./RovRemoteControl";
-// // import { NativeNavbar } from "./components/DevSandbox";
-// // import { DarkThemeToggle, Flowbite } from "flowbite-react";
-// import { FlowbiteWrapper, TopNavBar } from "./components/TopNavBar";
-// import { GpioAccess } from "./components/GpioUtils";
-// import { DeviceProvider } from "./AppStateContext";
-// import { EspApp } from "./EspApp";
 
 export const App = () => {
   return (<EspDashboard />);
